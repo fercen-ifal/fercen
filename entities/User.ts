85be21bd063efbc98d61799a46a557d27ce02913
Gepetojj <nmjoaopedro22@gmail.com>
2022-12-19 15:26:05 -0300

    feat(entities/User.ts): adição de novas propriedades na interface do usuário

--- entities/User.ts
+++ entities/User.ts
@@ -1,9 +1,44 @@
 import type { Permission } from "./Permissions";
 
-export interface User {
-	// TODO
+/**
+ * Interface de conexão do usuário com fontes externas de autenticação.
+ */
+export interface UserProviderConnection {
+	uid: string;
+	name: string;
+	email: string;
+	connectedAt: FirebaseFirestore.Timestamp;
 }
 
+/**
+ * Interface do usuário disponibilizada pelo banco de dados.
+ */
+export interface User {
+	// TODO
+	id: string;
+	fullname: string | null;
+	username: string;
+	email: string;
+	permissions: Permission[];
+	googleProvider?: UserProviderConnection;
+	microsoftProvider?: UserProviderConnection;
+}
+
+/**
+ * Interface do usuário exclusiva do banco de dados.
+ *
+ * @see {@link User}
+ */
+export interface DatabaseUser extends User {
+	/**
+	 * Hash bcrypt da senha do usuário, existente apenas no banco de dados.
+	 */
+	password: string;
+}
+
+/**
+ * Interface do usuário exclusiva do objeto de request da API.
+ */
 export interface ApiRequestUser {
 	// TODO: Adjust on User interface change
 	id?: string;
