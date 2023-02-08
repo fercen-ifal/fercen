731c6afabe59768f8012d4d14a3c696f6a0d74af
João Pedro <nmjoaopedro22@gmail.com>
2023-02-07 23:16:00 -0300

    feat(entities/permissions.ts): adição de labels para cada permissão disponível no sistema

--- entities/Permissions.ts
+++ entities/Permissions.ts
@@ -17,6 +17,25 @@ export const Permissions = {
 	"read:invite": "read:invite",
 } as const;
 
+/** Labels das permissões disponíveis para uso no sistema.  */
+export const PermissionsLabels = {
+	// User related
+	"create:user": "Criar usuário",
+	"read:user": "Ler usuário",
+	"update:user": "Atualizar usuário",
+	"update:user:other": "Atualizar outro usuário",
+	"read:user:other": "Ler outro usuário",
+	"read:user:list": "Listar usuários",
+
+	// Session related
+	"create:session": "Criar sessão",
+	"read:session": "Ler sessão",
+
+	// Invite related
+	"create:invite": "Criar convites",
+	"read:invite": "Ler convites",
+} as const;
+
 /** Permissões padrão para usuários anônimos.  */
 export const AnonymousUserPermissions = ["create:user", "create:session"] as const;
 
