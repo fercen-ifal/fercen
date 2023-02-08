d8d89b5e7e7fde4c1ed20ddf98f7babb4ca26561
João Pedro <nmjoaopedro22@gmail.com>
2023-02-08 10:49:33 -0300

    fix(app/globals.css): correção do bug nos formulários flutuantes

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="static sm:relative sm:flex-grow h-full">
-			<div className="static sm:absolute inset-0 h-full">{children}</div>
-		</div>
-	);
-}
